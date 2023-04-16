fd7bb06932df5eed64f063f9c3649876e0181fdf
unknown <thomasjjohnson9311@gmail.com>
2023-04-16 18:21:31 -0500

    string interpolation of city latitude & longitude

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 fetch(
-  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d6aac51f8f278c439b163e4e7767e456")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
-
-fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=d6aac51f8f278c439b163e4e7767e456"
+  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d6aac51f8f278c439b163e4e7767e456"
 )
+  .then((response) => response.json())
+  .then((citiesFound) => {
+    let firstCity = citiesFound[0];
+    console.log(firstCity.lat);
+    console.log(firstCity.lon);
+
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=d6aac51f8f278c439b163e4e7767e456`
+    );
+  })
+
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
